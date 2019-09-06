b1ba7c32a9ad74f38586e509ab0c7306482cde52
OscarRivas99 <00005218@uca.edu.sv>
2019-09-06 15:19:28 -0600

    labo1

---
+++ main1.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: uca
+ *
+ * Created on 6 de septiembre de 2019, 15:11
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
