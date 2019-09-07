0a025239d0620383cdd624d59a77e84524f6d32f
Alejandra Santos <00159619@uca.edu.sv>
2019-09-06 20:06:32 -0600

    labo1 1 ejercicio 2

--- main2.cpp
+++ main2.cpp
@@ -1,20 +1,23 @@
 //ejercicio 16 lab 1
+#include "pch.h"
 #include <iostream>
 using namespace std;
-int main ()
+int main()
 {
-    int n, a[10],contador;
-    cout << "Ingrese 10 enteros."<<endl;
-    for (int i=0; i<10;i++)
-    {
-        cout <<"Elemento "<<i<<endl; 
-	cin>>a[i];
-        contador=contador+a[i];
-    }
-    cout<<"La sumatoria es: "<<contador<<endl;
-    cout<<"El promedio es: "<<contador/10<<endl;
+	int n=0;
+	int a[10];
+	int contador=0;
+	cout << "Ingrese 10 enteros." << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Elemento " << i << endl;
+		cin >> a[i];
+		contador = contador + a[i];
+	}
+	cout << "La sumatoria es: " << contador << endl;
+	cout << "El promedio es: " << contador / 10 << endl;
 
 
-return 0;
+	return 0;
 }
 
